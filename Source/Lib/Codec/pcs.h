65980ba47fe28b283564950f250d895142b67f18
BlueSwordM <neutronpcxt@gmail.com>
2025-02-24 00:15:46 -0500

    feat: adding and enabling HBD-MD user controls

--- Source/Lib/Codec/pcs.h
+++ Source/Lib/Codec/pcs.h
@@ -1191,6 +1191,7 @@ typedef struct PictureControlSetInitData {
     uint8_t alt_lambda_factors;
     uint8_t sharp_tx;
     bool    alt_ssim_tuning;
+    uint8_t hbd_mds;
 } PictureControlSetInitData;
 
 /**************************************
