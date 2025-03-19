cff328da6c453d01097d5d8ccec7a3da70f4759c
BlueSwordM <neutronpcxt@gmail.com>
2025-03-18 22:39:34 -0400

    feat: adding spy-rd levels

--- Source/API/EbSvtAv1Enc.h
+++ Source/API/EbSvtAv1Enc.h
@@ -1066,11 +1066,12 @@ typedef struct EbSvtAv1EncConfiguration {
      * @brief Enable spy-rd, an alternate RD metric that biases towards sharpness/detail retention,
      * at the possible expense of increased blocking and banding
      * 0: disabled
-     * 1: enabled
+     * 1: full
+     * 2: partial (interpolation filter tweaks only)
      * Default is 0
      */
-    Bool spy_rd;
-    
+    uint8_t spy_rd;
+
     /**
      * @brief Prevent macroblocks from being boosted to very low q.
      *
@@ -1142,7 +1143,7 @@ typedef struct EbSvtAv1EncConfiguration {
      double ac_bias;
 
     /*Add 128 Byte Padding to Struct to avoid changing the size of the public configuration struct*/
-    uint8_t padding[128 - 8 * sizeof(Bool) - 13 * sizeof(uint8_t) - sizeof(int8_t) - sizeof(uint32_t) - 2 * sizeof(double)];
+    uint8_t padding[128 - 7 * sizeof(Bool) - 14 * sizeof(uint8_t) - sizeof(int8_t) - sizeof(uint32_t) - 2 * sizeof(double)];
 
 } EbSvtAv1EncConfiguration;
 
--- Source/App/app_config.c
+++ Source/App/app_config.c
@@ -1335,7 +1335,7 @@ ConfigEntry config_entry_psy[] = {
      set_cfg_generic_token},
     {SINGLE_INPUT,
      SPY_RD_TOKEN,
-     "[PSY] Alternative psychovisual rate distortion, default is 0 [0-1]",
+     "[PSY] Alternative psychovisual rate distortion pathways, default is 0 [0-2]; 1 = full, 2 = partial",
      set_cfg_generic_token},
     {SINGLE_INPUT,
      LOW_Q_TAPER_TOKEN,
