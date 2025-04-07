365b3c828328b5d33db859253585ac0f6c8d5546
BlueSwordM <neutronpcxt@gmail.com>
2025-04-06 22:30:24 -0400

    fix 1: removal of default tune 3 noise norm strength; default now 1

--- Source/Lib/Globals/enc_settings.c
+++ Source/Lib/Globals/enc_settings.c
@@ -1116,7 +1116,7 @@ EbErrorType svt_av1_set_default_params(EbSvtAv1EncConfiguration *config_ptr) {
     config_ptr->adaptive_film_grain               = TRUE;
     config_ptr->tf_strength                       = 1;
     config_ptr->kf_tf_strength                    = 1;
-    config_ptr->noise_norm_strength               = 0;
+    config_ptr->noise_norm_strength               = 1;
     config_ptr->psy_rd                            = 0.5;
     config_ptr->spy_rd                            = 0;
     config_ptr->sharp_tx                          = 1;
