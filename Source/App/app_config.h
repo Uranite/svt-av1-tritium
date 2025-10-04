5c2872e5dc41ecdfff5072346e8e0421939cb71c
Uranite <62639703+Uranite@users.noreply.github.com>
2025-10-04 20:09:18 +0700

    FFMS2 from Essential

--- Source/App/app_config.h
+++ Source/App/app_config.h
@@ -126,6 +126,7 @@ typedef struct EbConfig {
      * File I/O
      ****************************************/
     FILE      *input_file;
+    char      *input_file_path;
     MemMapFile mmap; //memory mapped file handler
     bool       input_file_is_fifo;
     FILE      *bitstream_file;
@@ -200,6 +201,11 @@ typedef struct EbConfig {
 #endif
 
     char *fgs_table_path;
+
+    bool  use_ffms2;
+    void *ffms_video_source;
+    void *ffms_index;
+    int   ffms_track_num;
 } EbConfig;
 
 typedef struct EncChannel {
