b31a20824d0c41c70c7cbfc36b20c2c2ed85f9f5
Uranite <62639703+Uranite@users.noreply.github.com>
2025-10-04 21:20:31 +0700

    SCD from Essential
    We keep 10 second gop (max 305) from svt-av1-hdr
    We also keep keyint -2
    Enabled by default
    Allow manually set min keyint with infinite keyint and scd

--- Source/API/EbSvtAv1Enc.h
+++ Source/API/EbSvtAv1Enc.h
@@ -232,6 +232,16 @@ typedef struct EbSvtAv1EncConfiguration {
      * Default is -2. */
     int32_t intra_period_length;
 
+    /* The min intra period defines the interval of frames before which a new
+     * Intra refresh can be inserted. It is strongly recommended to set the
+     * value to a multiple of the mini-gop size.
+     *
+     *  0 = no minimum (only relevant when scd=1).
+     * -1 = auto.
+     *
+     * Default is -1. */
+    int32_t min_intra_period_length;
+
     /* Random access.
      *
      * 1 = CRA, open GOP.
@@ -1110,7 +1120,7 @@ typedef struct EbSvtAv1EncConfiguration {
         - (sizeof(bool) * 4)
         - (sizeof(double) * 1)
         - (sizeof(int8_t) * 1)
-        - (sizeof(int32_t) * 1)
+        - (sizeof(int32_t) * 2)
     ];
     // clang-format on
 } EbSvtAv1EncConfiguration;
