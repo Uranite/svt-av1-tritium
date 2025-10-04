e3b915fa2b5bd9f81e20f0e3a7fd02240ed6e26d
Uranite <62639703+Uranite@users.noreply.github.com>
2025-10-04 21:20:31 +0700

    SCD from Essential
    We keep 10 second gop (max 305) from svt-av1-hdr
    We also keep keyint -2
    Enabled by default

--- Source/API/EbSvtAv1Enc.h
+++ Source/API/EbSvtAv1Enc.h
@@ -244,6 +244,16 @@ typedef struct EbSvtAv1EncConfiguration {
      * Default is -2. */
     int32_t intra_period_length;
 
+    /* The min intra period defines the interval of frames before which a new
+     * Intra refresh can be inserted. It is strongly recommended to set the
+     * value to a multiple of the mini-gop size.
+     *
+     *  0 = no minimum (only relevant when scd=1).
+     * -1 = auto.
+     *
+     * Default is -1. */
+    int32_t min_intra_period_length;
+
     /* Random access.
      *
      * 1 = CRA, open GOP.
@@ -1087,7 +1097,7 @@ typedef struct EbSvtAv1EncConfiguration {
      bool auto_tiling;
 
     /*Add 128 Byte Padding to Struct to avoid changing the size of the public configuration struct*/
-    uint8_t padding[128 - (sizeof(uint8_t) * 15) - (sizeof(double) * 2) - (sizeof(bool) * 9)];
+    uint8_t padding[128 - (sizeof(uint8_t) * 15) - (sizeof(double) * 2) - (sizeof(bool) * 9) - sizeof(int32_t)];
 } EbSvtAv1EncConfiguration;
 
 /**
