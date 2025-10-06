c5d3442e9512454c29fa4f2cbd74a248b1460a47
Uranite <62639703+Uranite@users.noreply.github.com>
2025-10-06 17:57:03 +0700

    Rebrand to SVT-AV1-Tritium
    credits to shenef for the name
    thanks shenef!

--- Source/Lib/Globals/enc_handle.c
+++ Source/Lib/Globals/enc_handle.c
@@ -5880,13 +5880,13 @@ EB_API const char *svt_av1_get_version(void) {
     return SVT_AV1_CVS_VERSION;
 }
 
-EB_API const char *svt_hdr_get_version(void) {
-    return SVT_AV1_HDR_RELEASE;
+EB_API const char *svt_tritium_get_version(void) {
+    return SVT_AV1_TRITIUM_RELEASE;
 }
 
 EB_API void svt_av1_print_version(void) {
     SVT_INFO("-------------------------------------------\n");
-    SVT_INFO("SVT [version]:\tSVT-AV1-HDR Encoder Lib %s \"Cyclonus\"\n", SVT_AV1_CVS_VERSION);
+    SVT_INFO("SVT [version]:\tSVT-AV1-Tritium Encoder Lib %s \"Cyclonus\"\n", SVT_AV1_CVS_VERSION);
     const char *compiler =
 #if defined(__clang__)
     __VERSION__ "\t"
