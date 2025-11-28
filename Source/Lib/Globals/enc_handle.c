Uranite/svt-av1-hdr

SCD from Essential
We keep 10 second gop (max 305) from svt-av1-hdr
We also keep keyint -2
Enabled by default

Source/Lib/Globals/enc_handle.c
@@ -2490,6 +2490,21 @@ static int32_t compute_default_intra_period(
     return intra_period;
 }
 
+static int32_t compute_default_min_intra_period(
+    SequenceControlSet       *scs){
+    int32_t min_intra_period           = 0;
+    EbSvtAv1EncConfiguration   *config = &scs->static_config;
+    double fps                         = (double)scs->frame_rate / (1 << 16);
+    int32_t mini_gop_size              = (1 << (config->hierarchical_levels));
+
+    min_intra_period                   = (((int)(fps + mini_gop_size - 1) / mini_gop_size) * (mini_gop_size));
+
+    if (config->intra_refresh_type == 1)
+        min_intra_period -= 1;
+
+    return min_intra_period;
+}
+
 /*
 Calculates the default LAD value
 */
@@ -4230,7 +4245,7 @@ static void set_param_based_on_input(SequenceControlSet *scs)
 
     // Throws a warning when scene change is on, as the feature is not optimal and may produce false detections
     if (scs->static_config.scene_change_detection == 1)
-        SVT_WARN("Scene Change is not optimal and may produce suboptimal keyframe placements\n");
+        SVT_WARN("SCD has been optimized on SVT-AV1-Essential defaults. Accuracy cannot be guaranteed inside SVT-AV1-Skibidi.\n");
     // MRP level
     uint8_t mrp_level;
     if (scs->static_config.rtc) {
@@ -4332,6 +4347,7 @@ static void copy_api_from_app(
     // Padding Offsets
     scs->b64_size = 64;
     scs->static_config.intra_period_length = ((EbSvtAv1EncConfiguration*)config_struct)->intra_period_length;
+    scs->static_config.min_intra_period_length = ((EbSvtAv1EncConfiguration*)config_struct)->min_intra_period_length;
     scs->allintra = (scs->static_config.intra_period_length == 0);
     scs->static_config.multiply_keyint = config_struct->multiply_keyint;
     scs->static_config.intra_refresh_type = ((EbSvtAv1EncConfiguration*)config_struct)->intra_refresh_type;
@@ -4623,6 +4639,12 @@ static void copy_api_from_app(
             scs->static_config.frame_rate_denominator;
         scs->static_config.intra_period_length =
             (int32_t)(fps * scs->static_config.intra_period_length);
+    }
+    if (scs->static_config.intra_period_length == -1)
+        scs->static_config.min_intra_period_length = 0;
+    else {
+        if (scs->static_config.min_intra_period_length == -1)
+            scs->static_config.min_intra_period_length = compute_default_min_intra_period(scs);
     }
     if (scs->static_config.look_ahead_distance == (uint32_t)~0)
         scs->static_config.look_ahead_distance = compute_default_look_ahead(&scs->static_config);

Uranite/svt-av1-tritium

Source/Lib/Globals/enc_handle.c
@@ -2490,6 +2490,21 @@ static int32_t compute_default_intra_period(
     return intra_period;
 }
 
+static int32_t compute_default_min_intra_period(
+    SequenceControlSet       *scs){
+    int32_t min_intra_period           = 0;
+    EbSvtAv1EncConfiguration   *config = &scs->static_config;
+    double fps                         = (double)scs->frame_rate / (1 << 16);
+    int32_t mini_gop_size              = (1 << (config->hierarchical_levels));
+
+    min_intra_period                   = (((int)(fps + mini_gop_size - 1) / mini_gop_size) * (mini_gop_size));
+
+    if (config->intra_refresh_type == 1)
+        min_intra_period -= 1;
+
+    return min_intra_period;
+}
+
 /*
 Calculates the default LAD value
 */
@@ -4229,7 +4244,7 @@ static void set_param_based_on_input(SequenceControlSet *scs)
 
     // Throws a warning when scene change is on, as the feature is not optimal and may produce false detections
     if (scs->static_config.scene_change_detection == 1)
-        SVT_WARN("Scene Change is not optimal and may produce suboptimal keyframe placements\n");
+        SVT_WARN("SCD has been optimized on SVT-AV1-Essential defaults. Accuracy cannot be guaranteed inside SVT-AV1-Tritium.\n");
     // MRP level
     uint8_t mrp_level;
     if (scs->static_config.rtc) {
@@ -4329,6 +4344,7 @@ static void copy_api_from_app(SequenceControlSet *scs, EbSvtAv1EncConfiguration 
     // Padding Offsets
     scs->b64_size = 64;
     scs->static_config.intra_period_length = config_struct->intra_period_length;
+    scs->static_config.min_intra_period_length = config_struct->min_intra_period_length;
     scs->allintra = (scs->static_config.intra_period_length == 0);
     scs->static_config.multiply_keyint = config_struct->multiply_keyint;
     scs->static_config.intra_refresh_type = config_struct->intra_refresh_type;
@@ -4624,6 +4640,12 @@ static void copy_api_from_app(SequenceControlSet *scs, EbSvtAv1EncConfiguration 
             scs->static_config.frame_rate_denominator;
         scs->static_config.intra_period_length =
             (int32_t)(fps * scs->static_config.intra_period_length);
+    }
+    if (scs->static_config.intra_period_length == -1)
+        scs->static_config.min_intra_period_length = 0;
+    else {
+        if (scs->static_config.min_intra_period_length == -1)
+            scs->static_config.min_intra_period_length = compute_default_min_intra_period(scs);
     }
     if (scs->static_config.look_ahead_distance == (uint32_t)~0)
         scs->static_config.look_ahead_distance = compute_default_look_ahead(&scs->static_config);