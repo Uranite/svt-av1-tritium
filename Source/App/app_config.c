Uranite/svt-av1-hdr

Rebrand to SVT-AV1-Tritium
credits to shenef for the name
thanks shenef!

Source/App/app_config.c
@@ -1857,14 +1857,14 @@ int get_version(int argc, char *argv[]) {
 #endif
     if (find_token(argc, argv, VERSION_TOKEN, NULL))
         return 0;
-    printf("SVT-AV1-HDR %s (%s)\n", svt_av1_get_version(), debug_build ? "release" : "debug");
+    printf("SVT-AV1-Tritium %s (%s)\n", svt_av1_get_version(), debug_build ? "release" : "debug");
 #if defined(_WIN64) || defined(_MSC_VER) || defined(_WIN32)
-    printf("HDR Release: %s\n", svt_hdr_get_version());
+    printf("Tritium Release: %s\n", svt_tritium_get_version());
 #else
-    if (strcmp(svt_hdr_get_version(), "N/A")) {
-        printf("HDR Release: \x1b[32m%s\x1b[0m\n", svt_hdr_get_version());
+    if (strcmp(svt_tritium_get_version(), "N/A")) {
+        printf("Tritium Release: \x1b[32m%s\x1b[0m\n", svt_tritium_get_version());
     } else {
-        printf("HDR Release: \x1b[38;5;248m%s\x1b[0m\n", svt_hdr_get_version());
+        printf("Tritium Release: \x1b[38;5;248m%s\x1b[0m\n", svt_tritium_get_version());
     }
 #endif
     return 1;

Uranite/svt-av1-tritium

Source/App/app_config.c
@@ -1816,21 +1816,21 @@ int get_version(int argc, char *const argv[], bool color) {
 #endif
     if (find_token(argc, argv, VERSION_TOKEN, NULL))
         return 0;
-    printf("SVT-AV1-HDR %s (" BUILD_TYPE_STRING ")\n", svt_av1_get_version());
+    printf("SVT-AV1-Tritium %s (" BUILD_TYPE_STRING ")\n", svt_av1_get_version());
 #if defined(_WIN64) || defined(_MSC_VER) || defined(_WIN32)
-    printf("HDR Release: %s\n", svt_hdr_get_version());
+    printf("Tritium Release: %s\n", svt_hdr_get_version());
 #else
     if (strcmp(svt_hdr_get_version(), "N/A")) {
         if (color) {
-            printf("HDR Release: \x1b[32m%s\x1b[0m\n", svt_hdr_get_version());
+            printf("Tritium Release: \x1b[32m%s\x1b[0m\n", svt_hdr_get_version());
         } else {
-            printf("HDR Release: %s\n", svt_hdr_get_version());
+            printf("Tritium Release: %s\n", svt_hdr_get_version());
         }
     } else {
         if (color) {
-            printf("HDR Release: \x1b[38;5;248m%s\x1b[0m\n", svt_hdr_get_version());
+            printf("Tritium Release: \x1b[38;5;248m%s\x1b[0m\n", svt_hdr_get_version());
         } else {
-            printf("HDR Release: %s\n", svt_hdr_get_version());
+            printf("Tritium Release: %s\n", svt_hdr_get_version());
         }
     }
 #endif