4b5716ea9d407a8b1235d4761260155ebdee9b96
Uranite <62639703+Uranite@users.noreply.github.com>
2026-06-02 00:09:49 +0700

    More daala and daala fix

--- Source/Lib/Codec/product_coding_loop.c
+++ Source/Lib/Codec/product_coding_loop.c
@@ -1002,6 +1002,21 @@ static void fast_loop_core_pd0(ModeDecisionCandidateBuffer* cand_bf, PictureCont
                                                                           pcs->scs->static_config.ac_bias,
                                                                           pcs->scs->static_config.tx_bias)
             << 1;
+    if (ctx->tune_daala_level >= 4) {
+        const uint32_t qindex = pcs->ppcs->frm_hdr.quantization_params.base_q_idx;
+        *(cand_bf->fast_cost) = svt_spatial_full_distortion_daala_kernel(
+                                     input_pic->y_buffer,
+                                     input_origin_index,
+                                     input_pic->y_stride,
+                                     pred->y_buffer,
+                                     cu_origin_index,
+                                     pred->y_stride,
+                                     ctx->blk_geom->bwidth,
+                                     ctx->blk_geom->bheight,
+                                     ctx->hbd_md ? EB_TEN_BIT : EB_EIGHT_BIT,
+                                     qindex,
+                                     1);
+    }
     }
 }
 
@@ -1192,9 +1207,8 @@ static void obmc_trans_face_off(ModeDecisionCandidateBuffer* cand_bf, PictureCon
                                                                                      ctx->hbd_md ? EB_TEN_BIT : EB_EIGHT_BIT,
                                                                                      qindex,
                                                                                      1);
-                cand_bf->luma_fast_dist += daala_dist;
-                daala_dist <<= 4;
-                luma_fast_dist += daala_dist;
+                cand_bf->luma_fast_dist = daala_dist;
+                luma_fast_dist = daala_dist << 4;
             }
 
             // Fast Cost
@@ -1380,9 +1394,8 @@ void fast_loop_core(ModeDecisionCandidateBuffer* cand_bf, PictureControlSet* pcs
                                                                              ctx->hbd_md ? EB_TEN_BIT : EB_EIGHT_BIT,
                                                                              qindex,
                                                                              1);
-        cand_bf->luma_fast_dist += daala_dist;
-        daala_dist <<= 4;
-        luma_fast_dist += daala_dist;
+        cand_bf->luma_fast_dist = daala_dist;
+        luma_fast_dist = daala_dist << 4;
     }
 
     if (ctx->mds0_ctrls.pruning_method_th && ctx->pd_pass == PD_PASS_1) {
