2977c6567b3b8887702b5939e69873378139fc9c
Uranite <62639703+Uranite@users.noreply.github.com>
2026-06-10 11:57:57 +0700

    Move min_intra_period_length
    turns out you can't place it mid struct

--- Source/API/EbSvtAv1Enc.h
+++ Source/API/EbSvtAv1Enc.h
@@ -247,15 +247,6 @@ typedef struct EbSvtAv1EncConfiguration {
      * Default is -2. */
     int32_t intra_period_length;
 
-    /* The min intra period defines the interval of frames before which a new
-     * Intra refresh can be inserted. It is strongly recommended to set the
-     * value to a multiple of the mini-gop size.
-     *
-     *  0 = no minimum (only relevant when scd=1).
-     * -1 = auto.
-     *
-     * Default is -1. */
-    int32_t min_intra_period_length;
 
     /* Random access.
      *
@@ -1208,6 +1199,16 @@ typedef struct EbSvtAv1EncConfiguration {
      *  1: QM-PSNR.
      * */
     int8_t enable_qmpsnr;
+
+    /* The min intra period defines the interval of frames before which a new
+     * Intra refresh can be inserted. It is strongly recommended to set the
+     * value to a multiple of the mini-gop size.
+     *
+     *  0 = no minimum (only relevant when scd=1).
+     * -1 = auto.
+     *
+     * Default is -1. */
+    int32_t min_intra_period_length;
 
     /**
      * @brief Signal to the library to automatically adjust tiles
