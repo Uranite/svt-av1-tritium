juliobbv-p/svt-av1-hdr

Introduce Film Grain tune

Source/Lib/Globals/enc_settings.c
@@ -551,9 +551,10 @@ EbErrorType svt_av1_verify_settings(SequenceControlSet *scs) {
             config->fast_decode);
         return_error = EB_ErrorBadParameter;
     }
-    if (config->tune > TUNE_IQ) {
+    if (config->tune > TUNE_FILM_GRAIN) {
         SVT_ERROR(
-            "Instance %u: Invalid tune flag [0 - 3, 0 for VQ, 1 for PSNR, 2 for SSIM and 3 for IQ], your input: %d\n",
+            "Instance %u: Invalid tune flag [0 - 3, 0 for VQ, 1 for PSNR, 2 for SSIM, 3 for IQ, 4 for Film Grain], "
+            "your input: %d\n",
             channel_number + 1,
             config->tune);
         return_error = EB_ErrorBadParameter;
@@ -574,6 +575,13 @@ EbErrorType svt_av1_verify_settings(SequenceControlSet *scs) {
                 channel_number + 1);
         }
 #endif
+    }
+
+    if (config->tune == TUNE_FILM_GRAIN) {
+        SVT_WARN(
+            "Instance %u: This tune is optimized for content that has moderate to heavy film grain. "
+            "Keep in mind for benchmarking analysis that this configuration will likely harm metric performance.\n",
+            channel_number + 1);
     }
 
     if (config->superres_mode > SUPERRES_AUTO) {
@@ -850,9 +858,10 @@ EbErrorType svt_av1_verify_settings(SequenceControlSet *scs) {
     }
 
     if (config->pred_structure == LOW_DELAY) {
-        if (config->tune == TUNE_VQ) {
-            SVT_WARN("Instance %u: Tune 0 is not applicable for low-delay, tune will be forced to 1.\n",
-                     channel_number + 1);
+        if (config->tune == TUNE_VQ || config->tune == TUNE_FILM_GRAIN) {
+            SVT_WARN("Instance %u: Tune %i is not applicable for low-delay, tune will be forced to 1.\n",
+                     channel_number + 1,
+                     config->tune);
             config->tune = TUNE_PSNR;
         }
 
@@ -1190,7 +1199,8 @@ void svt_av1_print_lib_params(SequenceControlSet *scs) {
                  config->tune == TUNE_VQ         ? "VQ"
                      : config->tune == TUNE_PSNR ? "PSNR"
                      : config->tune == TUNE_SSIM ? "SSIM"
-                                                 : "IQ",
+                     : config->tune == TUNE_IQ   ? "IQ"
+                                                 : "Film Grain",
                  config->pred_structure == LOW_DELAY           ? "low delay"
                      : config->pred_structure == RANDOM_ACCESS ? "random access"
                                                                : "Unknown pred structure");

Uranite/svt-av1-tritium

Source/Lib/Globals/enc_settings.c
@@ -548,9 +548,10 @@ EbErrorType svt_av1_verify_settings(SequenceControlSet* scs) {
             config->fast_decode);
         return_error = EB_ErrorBadParameter;
     }
-    if (config->tune > TUNE_VMAF) {
+    if (config->tune > TUNE_FILM_GRAIN) {
         SVT_ERROR(
-            "Invalid tune flag [0 - 5, 0 for VQ, 1 for PSNR, 2 for SSIM, 3 for IQ, 4 for MS_SSIM, and 5 for VMAF], "
+            "Invalid tune flag [0 - 6, 0 for VQ, 1 for PSNR, 2 for SSIM, 3 for IQ, 4 for MS_SSIM, 5 for VMAF, and 6 "
+            "for Film Grain], "
             "your input: "
             "%d\n",
             config->tune);
@@ -593,6 +594,11 @@ EbErrorType svt_av1_verify_settings(SequenceControlSet* scs) {
         SVT_WARN("Tune IQ with low delay prediction structure is experimental\n");
     }
 
+    if (config->tune == TUNE_FILM_GRAIN) {
+        SVT_WARN(
+            "This tune is optimized for content that has moderate to heavy film grain. "
+            "Keep in mind for benchmarking analysis that this configuration will likely harm metric performance.\n");
+    }
     if (config->superres_mode > SUPERRES_AUTO) {
         SVT_ERROR("invalid superres-mode %d, should be in the range [%d - %d]\n",
                   config->superres_mode,
@@ -801,8 +807,8 @@ EbErrorType svt_av1_verify_settings(SequenceControlSet* scs) {
     }
 
     if (config->pred_structure == LOW_DELAY) {
-        if (config->tune == TUNE_VQ) {
-            SVT_WARN("Tune 0 is not applicable for low-delay, tune will be forced to 1.\n");
+        if (config->tune == TUNE_VQ || config->tune == TUNE_FILM_GRAIN) {
+            SVT_WARN("Tune %i is not applicable for low-delay, tune will be forced to 1.\n", config->tune);
             config->tune = TUNE_PSNR;
         }
 
@@ -1156,12 +1162,13 @@ void svt_av1_print_lib_params(SequenceControlSet* scs) {
 
         SVT_INFO("SVT [config]: preset / tune / pred struct \t\t\t\t\t: %d / %s / %s\n",
                  config->enc_mode,
-                 config->tune == TUNE_VQ            ? "VQ"
-                     : config->tune == TUNE_PSNR    ? "PSNR"
-                     : config->tune == TUNE_SSIM    ? "SSIM"
-                     : config->tune == TUNE_MS_SSIM ? "MS_SSIM"
-                     : config->tune == TUNE_VMAF    ? "VMAF"
-                                                    : "IQ",
+                 config->tune == TUNE_VQ               ? "VQ"
+                     : config->tune == TUNE_PSNR       ? "PSNR"
+                     : config->tune == TUNE_SSIM       ? "SSIM"
+                     : config->tune == TUNE_MS_SSIM    ? "MS_SSIM"
+                     : config->tune == TUNE_VMAF       ? "VMAF"
+                     : config->tune == TUNE_FILM_GRAIN ? "Film Grain"
+                                                       : "IQ",
                  config->pred_structure == LOW_DELAY           ? "low delay"
                      : config->pred_structure == RANDOM_ACCESS ? "random access"
                      : config->pred_structure == ALL_INTRA     ? "all intra"